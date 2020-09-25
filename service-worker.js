20143f6639f85f78279901117ea4838d2f0dd827
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-09-25 12:05:27 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.920fd09cf173fdc03b7173aececd11a3.js"
+  "/precache-manifest.05901896b68639e7b0e5c4a0abcde44b.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/11.b71cce41.chunk.js
+++
@@ -1,1 +0,0 @@
-(this.webpackJsonpyam=this.webpackJsonpyam||[]).push([[11],{529:function(p,s){}}]);
